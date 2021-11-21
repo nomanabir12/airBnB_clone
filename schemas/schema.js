a89beecebc9ea58390c578a4736024108d8cc70e
nomanabir12 <alnomanofficial@gmail.com>
2021-11-21 10:46:33 +0600

    My Home

--- schemas/schema.js
+++ schemas/schema.js
@@ -4,6 +4,8 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import property from './property'
+import propertyImage from './propertyImage'
+import review from './Review'
 
 
 export default createSchema({
@@ -11,6 +13,8 @@ export default createSchema({
   name: 'default',
   
   types: schemaTypes.concat([
-    property
+    property,
+    propertyImage,
+    review
   ]),
 })
